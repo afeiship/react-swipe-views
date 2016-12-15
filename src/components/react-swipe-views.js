c6a693027bb53dfa5019da79d82ceb18806bd5e8
feizheng <1290657123@qq.com>
2016-12-15 23:33:32 +0800

    ms error

--- src/components/react-swipe-views.js
+++ src/components/react-swipe-views.js
@@ -4,6 +4,7 @@ import Swipeable from 'react-swipeable';
 import Utils from './react-swipe-views-utils';
 import Measure from 'react-dimensions';
 
+
 class ReactSwipeViews extends React.Component{
   static propTypes = {
     cssClass:React.PropTypes.string,
@@ -21,16 +22,15 @@ class ReactSwipeViews extends React.Component{
     }
   };
 
-
   constructor(props) {
     super(props);
     this.state = {
-      activeIndex:props.activeIndex,
-      items:props.items,
-      itemTemplate:props.itemTemplate,
-      duration:props.duration,
-      translateX:0,
-      dimensions:{}
+      activeIndex: props.activeIndex,
+      items: props.items,
+      itemTemplate: props.itemTemplate,
+      duration: props.duration,
+      translateX: 0,
+      dimensions: {}
     };
     this._length = props.items.length;
   }
@@ -53,21 +53,23 @@ class ReactSwipeViews extends React.Component{
 
   render(){
     return (
-      
-        <Swipeable
-          className={classNames('react-swipe-views',this.props.cssClass)}
-          onSwipedLeft={this._onSwipedLeft.bind(this)}
-          onSwipedRight={this._onSwipedRight.bind(this)}
-          onSwipingLeft={this._onSwipingLeft.bind(this)}
-          onSwipingRight={this._onSwipingRight.bind(this)}>
-          <div className="react-swipe-views-wrapper">
+      <Measure onMeasure={(dimensions) => {
+          console.log(dimensions);
+          this.setState({dimensions})
+        }}>
+        <div className={classNames('react-swipe-views',this.props.cssClass)}>
+          <Swipeable className="react-swipe-views-wrapper"
+            onSwipedLeft={this._onSwipedLeft.bind(this)}
+            onSwipedRight={this._onSwipedRight.bind(this)}
+            onSwipingLeft={this._onSwipingLeft.bind(this)}
+            onSwipingRight={this._onSwipingRight.bind(this)}>
             <div className="react-swipe-views-scroller"
               style={{
                 width:`${100*this._length}%`,
                 transition:`transform ${this.state.duration}s`,
                 transform:`translateX(${this.state.translateX})`
               }}>
-              {this.state.items.map((item,index) =>{
+              {this.state.items.map((item,index) => {
                 return (
                     <div className="react-swipe-views-item"
                       key={index}
@@ -79,8 +81,9 @@ class ReactSwipeViews extends React.Component{
                 );
               })}
             </div>
-          </div>
-        </Swipeable>
+          </Swipeable>
+        </div>
+      </Measure>
     );
   }
 }
