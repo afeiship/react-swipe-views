a5a4b48fed1ed344a458ca95ffecd87bce3afd35
feizheng <1290657123@qq.com>
2016-12-17 19:49:35 +0800

    export base

--- src/main.js
+++ src/main.js
@@ -1,6 +1,8 @@
+import ReactSwipeViewsBase from 'components/react-swipe-views-base';
 import ReactSwipeViews from 'components/react-swipe-views';
 import ReactSwipeViewsInfinite from 'components/react-swipe-views-infinite';
 module.exports = {
+  ReactSwipeViewsBase,
   ReactSwipeViews,
   ReactSwipeViewsInfinite
 };
