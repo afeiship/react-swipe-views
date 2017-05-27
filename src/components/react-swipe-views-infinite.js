c5383b785141a1f5a94171eaf1b21e0298e5cfa4
afeiship <1290657123@qq.com>
2017-05-27 18:28:27 +0800

    bugfix

--- src/components/react-swipe-views-infinite.js
+++ src/components/react-swipe-views-infinite.js
@@ -3,11 +3,15 @@ import ReactSwipeViewsBase from './react-swipe-views-base';
 export default class extends ReactSwipeViewsBase {
 
   processItems(inProps) {
+    const {followFinger} = inProps;
     let items = inProps.children;
     let first = items[0];
     let last = items[items.length - 1];
-    items.push(first);
-    items.unshift(last);
+
+    if(followFinger){
+      items.push(first);
+      items.unshift(last);
+    }
 
     this._index = inProps.activeIndex + 1;
     this._length = inProps.children.length;
